217af980ebaa2a9ed5602294e01aff61934ede5f
nicolas.heldenbergh@gmail.com <nicolas.heldenbergh@gmail.com>
2023-09-29 14:07:00 +0200

    Commit

--- src/Weather/AppleIOSCurrentConditionsDisplay.java
+++ src/Weather/AppleIOSCurrentConditionsDisplay.java
@@ -1,7 +1,7 @@
 package Weather;
 
 // This is an observer that displays the current weather conditions
-public class AppleIosCurrentConditionsDisplay implements Observable {
+public class AppleIOSCurrentConditionsDisplay implements Observable {
     @Override
     public void update(float temperature, float humidity) {
         System.out.println("Current conditions: " + temperature + " degrees and " + humidity + "% humidity");
--- src/Weather/WeatherStation.java
+++ src/Weather/WeatherStation.java
@@ -7,7 +7,7 @@ public class WeatherStation {
         WeatherData weatherData = new WeatherData();
 
         // Create observers
-        AppleIosCurrentConditionsDisplay currentDisplay = new AppleIosCurrentConditionsDisplay();
+        AppleIOSCurrentConditionsDisplay currentDisplay = new AppleIOSCurrentConditionsDisplay();
         RTBFTvChannelForecastDisplay forecastDisplay = new RTBFTvChannelForecastDisplay();
 
         // Register observers with the subject
